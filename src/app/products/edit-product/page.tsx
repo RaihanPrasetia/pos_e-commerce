ba9329b06187b2f54ba60784767b731ad26240e2
RaihanPrasetia <raihanprasetia86@gmail.com>
2024-12-24 06:24:17 +0700

    fix bug

--- src/app/products/edit-product/page.tsx
+++ src/app/products/edit-product/page.tsx
@@ -1,19 +1,24 @@
-import General from '@/components/product/edit/General'
-import Organize from '@/components/product/edit/Organize'
-import ProductImage from '@/components/product/edit/ProductImage'
-import ProductThumbnail from '@/components/product/edit/ProductThumbnail'
-import Varian from '@/components/product/edit/Varian'
-import React from 'react'
+"use client";
+
+
+import dynamic from 'next/dynamic'
+
+// Dynamically import components with server-side rendering disabled (only rendered on client-side)
+const General = dynamic(() => import('@/components/product/edit/General'), { ssr: false })
+const ProductImage = dynamic(() => import('@/components/product/edit/ProductImage'), { ssr: false })
+const Varian = dynamic(() => import('@/components/product/edit/Varian'), { ssr: false })
+const ProductThumbnail = dynamic(() => import('@/components/product/edit/ProductThumbnail'), { ssr: false })
+const Organize = dynamic(() => import('@/components/product/edit/Organize'), { ssr: false })
 
 export default function EditProduct() {
     return (
-        <div className='flex items-start justify-between gap-8 '>
-            <div className='w-3/5 space-y-8'>
+        <div className="flex items-start justify-between gap-8">
+            <div className="w-3/5 space-y-8">
                 <General />
                 <ProductImage />
                 <Varian />
             </div>
-            <div className='w-2/5 space-y-8'>
+            <div className="w-2/5 space-y-8">
                 <ProductThumbnail />
                 <Organize />
             </div>
